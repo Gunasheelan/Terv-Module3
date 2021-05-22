90c78ed045469623a2dd5fd14217a1ba3c168c4c
Gunasheelan <gunasheelan21@gmail.com>
2021-05-22 18:15:41 +0530

    commit

---
+++ EmailValidation.java
@@ -0,0 +1,9 @@
+import java.util.*;
+import java.util.regex.Pattern;
+public class Main{
+  public static void main(String[] args){
+    Scanner scanner = new Scanner(System.in);
+    String word = scanner.nextLine();
+    System.out.println(Pattern.matches("^[a-zA-Z0-9_-]+@[a-z]+.[a-z]+$",word));
+  }
+}
