fd09299eaccc30032b9c64a1b699ceb696bd680c
Gunasheelan <gunasheelan21@gmail.com>
2021-05-24 11:27:55 +0530

    commit

---
+++ SquareAndReverse.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main{
+  public static void main(String[] args){
+    Scanner scanner = new Scanner(System.in);
+    int num = scanner.nextInt();
+    int sq = num*num;
+    System.out.println((float)sq);
+    System.out.println(sq);
+    int result = 0;
+    while(sq != 0){
+      result = result * 10 + sq % 10;
+      sq = sq / 10;
+    }
+    System.out.println(result);
+  }
+}
