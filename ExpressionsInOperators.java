e4a7ace943afc6884477d5a3233c12bb9decf20a
Gunasheelan <gunasheelan21@gmail.com>
2021-05-27 17:19:17 +0530

    commit

---
+++ ExpressionsInOperators.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main{
+  public static void main(String[] args){
+    Scanner scanner = new Scanner(System.in);
+    int a = scanner.nextInt();
+    int b = scanner.nextInt();
+    int c = scanner.nextInt();
+    int d = scanner.nextInt();
+    int e = scanner.nextInt();
+    int f = scanner.nextInt();
+    
+    System.out.println("a+b/d = "+(a +b / d));
+    System.out.println("a+b*d-e/f = "+(a + b*d-e/f));
+  }	
+}
