2ccbe4b3cf3a77fc6038dce8c0628fb07557ab14
Gunasheelan <gunasheelan21@gmail.com>
2021-05-28 10:53:37 +0530

    commit

---
+++ UnsignedRightShift.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main{
+  public static void main(String[] args){
+    Scanner scanner = new Scanner(System.in);
+    int a = scanner.nextInt();
+    System.out.println(a>>2);
+    System.out.println(a>>3);
+    System.out.println(a>>4);
+  }
+}
